fe0122ef98ab3eda60730f448c8b21707c970f87
ghastlith <72772611+ghastlith@users.noreply.github.com>
2023-11-11 21:04:54 -0300

    chore: initial commit

---
+++ src/test/java/blarknes/whoisidentifier/WhoisIdentifierApplicationTests.java
@@ -0,0 +1,13 @@
+package blarknes.whoisidentifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoisIdentifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
