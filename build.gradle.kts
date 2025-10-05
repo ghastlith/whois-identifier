4b8b33a53512d46aa4146f04bf4fad98f7a7d8f2
ghastlith <72772611+ghastlith@users.noreply.github.com>
2025-10-05 09:23:29 -0300

    fix: change gradle language from groovy to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "blarknes"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+dependencies {
+    // spring
+    implementation("org.springframework.boot:spring-boot-starter-web:3.5.4")
+
+    // tests
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.5.4")
+    testImplementation("org.assertj:assertj-core:3.24.2")
+
+    // lombok
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+    testCompileOnly("org.projectlombok:lombok:1.18.38")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
